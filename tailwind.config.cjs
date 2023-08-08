b236dc717cc389333350ede15a9b0e462232df32
Chris Galbraith <chris@resknow.co.uk>
2023-08-08 09:34:38 +0100

    Update templates and docs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./src/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/**/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	theme: {
 		container: {
